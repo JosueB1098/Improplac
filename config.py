fc4da19a8e3a87c9427cfcae4755a774932db28f
Josue Bejarano <josuebejarano2015@gmail.com>
2024-12-02 22:04:15 -0500

    Nuevo con base

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 
 class Config(object):
     SECRET_KEY = 'admin'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/improplac'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/improplac'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://improplac_user:NsPmkfuP7CEeSzttjRk1to8tjnUYuY1b@dpg-ct775jd2ng1s73cas1cg-a.ohio-postgres.render.com/improplac'
